25eebfefb1c04f377a2bd3393e9b4885289715d8
Dave Lemmens <bricknpc@proton.me>
2025-11-12 23:18:28 +0100

    Add config file and view location

---
+++ config/eloquent-tables.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Eloquent Tables Configuration
+ * ================================
+ *
+ * Configuration options for the Eloquent Tables package.
+ */
+return [];
--- src/Providers/EloquentTablesServiceProvider.php
+++ src/Providers/EloquentTablesServiceProvider.php
@@ -4,4 +4,17 @@ declare(strict_types=1);
 
 namespace BrickNPC\EloquentTables\Providers;
 
-class EloquentTablesServiceProvider {}
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class EloquentTablesServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        View::addNamespace('eloquent-tables', __DIR__ . '/../../resources/views');
+
+        $this->mergeConfigFrom(__DIR__ . '/../../config/eloquent-tables.php', 'eloquent-tables');
+    }
+
+    public function register(): void {}
+}
