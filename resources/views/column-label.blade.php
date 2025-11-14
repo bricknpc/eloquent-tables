7a0ce270050d367184adf4121d2c7f175121ba4f
Dave Lemmens <bricknpc@proton.me>
2025-11-14 02:00:18 +0100

    Add sorting options to column header

--- resources/views/column-label.blade.php
+++ resources/views/column-label.blade.php
@@ -1,7 +1,17 @@
-<div class="d-flex justify-content-between">
+<div class="d-flex justify-content-between align-items-center">
     @if($sortable)
-        <span>{{ $label }}</span>
-        <x-bi-sort-alpha-down class="text-muted" />
+        <a href="{{ $href }}" class="d-flex justify-content-between align-items-center">
+            <span>{{ $label }}</span>
+            @if (!$isSorted)
+                <x-bi-sort-alpha-down class="text-muted" />
+            @else
+                @if($sortDirection === \BrickNPC\EloquentTables\Enums\Sort::Asc)
+                    <x-bi-sort-alpha-up />
+                @else
+                    <x-bi-sort-alpha-up />
+                @endif
+            @endif
+        </a>
     @else
         {{ $label }}
     @endif
