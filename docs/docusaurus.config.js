2b8b35b65bd6054b5233e586244fb5f6c0517181
Dave Lemmens <bricknpc@proton.me>
2025-11-21 06:15:18 +0100

    Fix Docusaurus config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/',
+  url: 'https://bricknpc.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/bricknpc/eloquent-tables',
+  baseUrl: '/eloquent-tables',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
