1b8b64c6506629e317b39e44f51556b2c57ae7da
Dave Lemmens <bricknpc@proton.me>
2025-12-13 01:06:35 +0100

    Add execution of action type to action

--- src/Actions/Contracts/Authorizable.php
+++ src/Actions/Contracts/Authorizable.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace BrickNPC\EloquentTables\Actions\Concerns;
+namespace BrickNPC\EloquentTables\Actions\Contracts;
 
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
@@ -10,9 +10,11 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @template TModel of Model
  *
+ * @extends GuardsAction<TModel>
+ *
  * @phpstan-type AuthorizeCallback \Closure(Request $request, ?TModel $model): bool
  */
-interface Authorizable
+interface Authorizable extends GuardsAction
 {
     /**
      * @var null|AuthorizeCallback $authorize
